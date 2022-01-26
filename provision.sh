edf5d6804818397e60eb6624a64117970b1bc751
Dylan <me@dylanmassey.ch>
2022-01-26 11:11:43 +0100

    updated provisioner

--- provision.sh
+++ provision.sh
@@ -19,12 +19,12 @@ function executeCowsay() {
 
 # -------------------------- HELPER FUNCTIONS -----------------------------
 function PutSymlink() {
-	if [ -d "$1" ] || [ -f "$1" ]; then
+	if [[ -d "$1" || -f "$1" ]]; then
 		read "REPLY?A directory already exists for $2, do you want to overwrite it?"
 		echo
 		if [[ $REPLY =~ ^[Yy]$ ]]
 		then
-			"$3"
+			rm $1 && ln -s -f $3 $4
 		fi
 	fi
 }
@@ -36,49 +36,49 @@ function start() {
 
 	# Show hidden files
 	echo "Setting hidden files to show..."
-	(defaults write com.apple.Finder AppleShowAllFiles true && killall Finder)
+	# (defaults write com.apple.Finder AppleShowAllFiles true && killall Finder)
 
 	# - HOMEBREW INSTALLATION -
 	echo "Installing homebrew..."
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-	(echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/dylan/.zprofile && eval "$(/opt/homebrew/bin/brew shellenv)")
-
-	(brew tap Homebrew/bundle)
+	# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 	# Install applications specified in the brewfile
 	echo "Installing brewfile specifications..."
 	(cp ~/Dropbox/Apps/Homebrew/Brewfile . && brew bundle)
 
 	echo "Testing cowsay..."
-	installCowsay
-	executeCowsay
-	uninstallCowsay
+	#installCowsay
+	#executeCowsay
+	#uninstallCowsay
 
 	# Import sublime settings
 	echo "Provisioning sublime settings..."
-	(cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/ && rm -r User && ln -s ~/Dropbox/Apps/Sublime/User)
+	# (cd ~/Library/Application\ Support/Sublime\ Text/Packages/ && rm -r User && ln -s ~/Dropbox/Apps/Sublime/User)
 
 	# Installing OhMyZsh
 	echo "Installing Ohmyzsh..."
-	(sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
+	# (sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
+	
+	echo "Installing zsh plugins"
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 	# Symlinking dotfiles
 	echo "Symlinking dotfiles..."
 
 	echo "[1] /.ssh"
 	DIR="$HOME/.ssh"
-	PutSymlink $DIR "ssh keys" "(ln -s -f ~/Dropbox/Apps/dot/ssh ~/.ssh)"
+	PutSymlink $DIR "ssh keys" ~/Dropbox/Apps/dot/ssh ~/.ssh
 
 
 	echo "[2] .zshrc"
 	DIR="$HOME/.zshrc"
-	PutSymlink $DIR "zshrc" "(ln -s -f ~/Dropbox/Apps/dot/zshrc ~/.zshrc)"
+	PutSymlink $DIR "zshrc" ~/Dropbox/Apps/dot/zshrc ~/.zshrc
 
-
+	
 	echo "[3] .zsh_history"
 	DIR="$HOME/.zsh_history"
-	PutSymlink $DIR "zsh_history" "(ln -s -f ~/Dropbox/Apps/dot/zsh_history ~/.zsh_history)"
+	PutSymlink $DIR "zsh_history" ~/Dropbox/Apps/dot/zsh_history ~/.zsh_history
+
 
 
 }
