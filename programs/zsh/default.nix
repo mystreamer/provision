648759e557643c984b6fdb9c09447c78d3c46518
Dylan <me@dylanmassey.ch>
2025-05-28 16:49:36 +0200

    devnix: onfig zsh-shell

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -1,7 +1,15 @@
 { pkgs, config, lib, inputs, ... }:
   {
-    programs.vim = {
+    programs.zsh = {
       enable = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "amuse";
+        plugins = [
+          "zsh-autosuggestions"
+          "git"
+        ];
+      } ;
       initContent = ''
       . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
       '';
