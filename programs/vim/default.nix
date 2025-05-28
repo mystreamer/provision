3838852b0b9694d0855f16ddd0315a432ed97b3c
Dylan <me@dylanmassey.ch>
2025-05-28 13:53:59 +0200

    Add: new vim plugins

--- programs/vim/default.nix
+++ programs/vim/default.nix
@@ -3,21 +3,50 @@
     programs.vim = {
       enable = true;
       plugins = with pkgs.vimPlugins; [ 
-        vim-airline 
         coc-nvim 
-        vim-slime
+        LanguageClient-neovim
         fzf-vim
+        vim-airline 
+        vim-slime
         dracula-vim
         windsurf-vim
+        nerdtree
         ];
       settings = { ignorecase = true; };
-      extraConfig = ''
+      extraConfig = '' 
+      " general settings
       colorscheme dracula
-
+      set hlsearch
+      set bs=2
+      set ai
+      set ruler
+      set hidden " needed for rename operations over multiple buffers in language server
       set mouse=a
 
+      " May need for Vim (not Neovim) since coc.nvim calculates byte offset by count
+      " utf-8 byte sequence
+      set encoding=utf-8
+      " Some servers have issues with backup files, see #649
+      set nobackup
+      set nowritebackup
+
+      " Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
+      " delays and poor user experience
+      set updatetime=300
+
+      " Always show the signcolumn, otherwise it would shift the text each time
+      " diagnostics appear/become resolved
+      set signcolumn=yes
+
+      " slime settings
       let g:slime_target = "tmux"
       let g:slime_paste_file = "$HOME/.slime_paste"
+
+      " LanguageClient settings
+      set rtp+=~/.vim/pack/XXX/start/LanguageClient-neovim
+      let g:LanguageClient_serverCommands = {
+        'haskell': ['haskell-language-server-wrapper', '--lsp'],
+      }
       '';
     };   
   }
