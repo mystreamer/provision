cf451998a666a5bc8da645b22bc51236378942de
Dylan <me@dylanmassey.ch>
2026-01-07 22:02:48 +0100

    add ports

--- programs/services/lyrion.nix
+++ programs/services/lyrion.nix
@@ -12,11 +12,18 @@
   
   virtualisation.oci-containers.containers.lyrion = {
     image = "lmscommunity/lyrionmusicserver:latest";
-    ports = [ "0.0.0.0:9000:9000" ];
+    ports = [ 
+      "0.0.0.0:9000:9000/tcp"
+      "0.0.0.0:9090:9090/tcp"
+      "0.0.0.0:3483:3483/tcp"
+      "0.0.0.0:3483:3483/udp"
+    ];
     volumes = [
-      "/var/lib/lyrion/config:/config"
-      "/var/lib/lyrion/music:/music"
-      "/var/lib/lyrion/playlists:/playlists"
+      "/var/lib/lyrion/config:/config:rw"
+      "/var/lib/lyrion/music:/music:ro"
+      "/var/lib/lyrion/playlists:/playlist:rw"
+      "/etc/localtime:/etc/localtime:ro"
+      "/etc/timezone:/etc/timezone:ro"
     ];
   };
 }
