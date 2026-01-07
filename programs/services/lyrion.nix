90654821233a692fbf7c0218b9ce65fd6d064e04
Dylan <me@dylanmassey.ch>
2026-01-07 21:55:00 +0100

    Fix: failed lyrion build

--- programs/services/lyrion.nix
+++ programs/services/lyrion.nix
@@ -1,10 +1,17 @@
 { config, ... }:
 
 {
+  systemd.tmpfiles.rules = [
+    "d /var/lib/lyrion 0755 root root -"
+    "d /var/lib/lyrion/config 0755 root root -"
+    "d /var/lib/lyrion/music 0755 root root -"
+    "d /var/lib/lyrion/playlists 0755 root root -"
+  ];
+
   virtualisation.oci-containers.backend = "docker";
   
   virtualisation.oci-containers.containers.lyrion = {
-    image = "ghcr.io/lms-community/lyrionmusicserver:latest";
+    image = "lmscommunity/lyrionmusicserver:latest";
     ports = [ "0.0.0.0:9000:9000" ];
     volumes = [
       "/var/lib/lyrion/config:/config"
